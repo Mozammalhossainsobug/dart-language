ff2438663882ca9e97ad05259d176bc2c72f86ac
Mozammal Hossain <38386041+Mozammalhossainsobug@users.noreply.github.com>
2022-10-03 14:57:36 +0600

    Update 3_changing_variable.dart

--- 3_changing_variable.dart
+++ 3_changing_variable.dart
@@ -1,12 +1,20 @@
 import 'dart:io';
 
 void main() {
-  String a = "10.2567";
-  // changing string to double
-  double b = double.parse(a);
+  // string -> double
+  double a = double.parse("10.2567");
+  print(a);
+
+  // string -> int
+  var b = int.parse("20");
   print(b);
 
-  // changing to int
-  var c = int.parse("20");
-  print(c);
+  // int -> string
+  var str1 = 3.toString();
+  print(str1);
+
+  // double -> string
+  String str2 = 3.16178.toStringAsFixed(2);
+  print(str2);
+  
 }
