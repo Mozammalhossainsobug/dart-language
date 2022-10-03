4ca672865551d5f65ea28562f6108a733b055237
Mozammal Hossain <38386041+Mozammalhossainsobug@users.noreply.github.com>
2022-10-03 14:42:50 +0600

    Create 4_string.dart

---
+++ 4_string.dart
@@ -0,0 +1,13 @@
+void main() {
+  // To write multiple line """qoutes""" should be given
+  var a = """
+  hello bello
+  it is a multiline string
+  """;
+
+  // To write some syntax "r" should be used
+  var b = r"We will write some \n letter here";
+
+  print(a);
+  print(b);
+}
