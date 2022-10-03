878d9f7dd43d197b9adf1182ba40d420ad7cf6d4
Mozammal Hossain <38386041+Mozammalhossainsobug@users.noreply.github.com>
2022-10-03 14:35:43 +0600

    Create 3_changing_variable.dart

---
+++ 3_changing_variable.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  String a = "10.2567";
+  // changing string to double
+  double b = double.parse(a);
+  print(b);
+
+  // changing to int
+  var c = int.parse("20");
+  print(c);
+}
